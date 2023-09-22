4f38e4d01cff3f3c8c6cdedae31b299eaf225bae
bokarmy6806 <88885@glr.nl>
2023-09-22 12:24:05 +0200

    begin

---
+++ app.js
@@ -0,0 +1,12 @@
+function projectenAnimation() {
+    let projecten = document.querySelectorAll("project");
+  
+    for (let i = 0; i < projecten.length; i++) {
+      projecten[i].style.animation = `slideLoad 0.2s ease ${i * 0.05}s forwards`;
+    }
+  }
+  
+  function reload() {
+    location.reload();
+  }
+  
