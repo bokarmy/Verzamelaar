c24d563b107bde08e61cc4efff8aacd2f36e8152
bokarmy6806 <88885@glr.nl>
2023-09-29 12:10:32 +0200

    brt

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-function projectenAnimation() {
-    let projecten = document.querySelectorAll("project");
-  
-    for (let i = 0; i < projecten.length; i++) {
-      projecten[i].style.animation = `slideLoad 0.2s ease ${i * 0.05}s forwards`;
-    }
+document.getElementById('projectForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Voorkom standaardformuliergedrag
+
+  // Haal de geselecteerde categorie op
+  const categorySelect = document.querySelector('.category-select');
+  const selectedCategory = categorySelect.value;
+
+  // Haal de projectinformatie op uit het formulier
+  const title = this.title.value;
+  const description = this.desc.value;
+  const date = this.datum.value;
+  const url = this.url.value;
+  const image = this.image.value;
+
+  // Maak een nieuw projectelement aan
+  const projectElement = document.createElement('div');
+  projectElement.innerHTML = `<strong>${title}</strong><br>
+                              Beschrijving: ${description}<br>
+                              Datum: ${date}<br>
+                              URL: ${url}<br>
+                              Foto URL: ${image}`;
+
+  // Voeg het projectelement toe aan de juiste container op basis van de categorie
+  if (selectedCategory === 'Persoonlijk') {
+      document.getElementById('personalProjectsContainer').appendChild(projectElement);
+  } else if (selectedCategory === 'School') {
+      document.getElementById('schoolProjectsContainer').appendChild(projectElement);
   }
-  
-  function reload() {
-    location.reload();
-  }
-  
+
+  // Reset het formulier
+  this.reset();
+});
